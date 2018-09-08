664fe7a0666ff6ade113699d077becd8344efc95
HILMI SAHIN <htolgasahin@gmail.com>
2018-09-08 16:36:08 +0200

    Implemented first successfull attempt

--- src/app.js
+++ src/app.js
@@ -1,18 +1,24 @@
 const Scene = require('class/Scene')
-const Sprite = require('class/Sprite')
+const GameObject = require('class/GameObject')
+const Animation = require('class/Animation')
 const Vector = require('class/Vector')
 const scene = new Scene()
 scene.canvas.width=800
 scene.addToBody()
 
-const Player = new Sprite({
-    url:'player_idle.png',
+const Player = new GameObject({
+    name:'Player',
     scene,
     width:40,
     height:40,
     position: new Vector(100,100)
 })
-Player.animate()
+const anim1 = new Animation(Player,{url:'player_idle.png'})
+anim1.play()
+setTimeout(()=>{
+    console.log('Stopped')
+    anim1.pause()
+},1000)
 
 const loop = () => {
     scene.clear()
